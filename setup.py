48ab85d74360d52b677a23db1bb4d8059ed523e1
cesar vargas <cesar@vargas.dev>
2020-06-08 16:24:49 -0300

    0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="oplab-client-python",
-    version="0.2.0",
+    version="0.2.1",
     author="César Vargas",
     author_email="cesar@oplab.com",
     description="Oplab's API Client Package",
